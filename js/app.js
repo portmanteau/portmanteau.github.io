567c7c26134f54e58c22c1cadc07daa003ca1c1e
Andy Aylward <aaylward@gmail.com>
2014-10-12 22:13:14 -0400

    js at the end

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 (function ($, global, undefined){
   // yo, fake pages
-  $('nav a').on('click, touch', function(event) {
+  $('header.navbar a').on('click', function(event) {
     console.log("event: ", event);
   })
-  
 }).call(null, jQuery, (1, eval)("this;"))
 
