230cbe0c65f69fd63ebe8cbec96cb70b8bd95862
Andy Aylward <aaylward@gmail.com>
2014-10-12 21:57:23 -0400

    js!

---
+++ js/app.js
@@ -0,0 +1,8 @@
+(function ($, global, undefined){
+  // yo, fake pages
+  $('nav a').on('click, touch', function(event) {
+    console.log("event: ", event);
+  })
+  
+}).call(null, jQuery, (1, eval)("this;"))
+
